168e94caff3567be7017e2d242aed083a625eec0
Maxwell Rupp <maxwell@giglab.dev>
2026-05-30 21:53:15 -0400

    init commit

---
+++ flake.nix
@@ -0,0 +1,8 @@
+{
+  description = "Gig's nixpkgs overlay";
+  inputs = {
+    nixpkgs = "github:NixOS/nixpkgs/nixpkgs-26.05";
+    nixpkgs-unstable = "github:NixOS/nixpkgs/master";
+    nixospkgs = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+}
