8ec39df5e5453ec53aa14d26bf27cb37ac6a14af
Maxwell Rupp <maxwell@giglab.dev>
2026-07-16 16:34:04 -0400

    added news entry

---
+++ news/entries/2026-07-16-test-slug.nix
@@ -0,0 +1,10 @@
+{
+  id = "2026-07-16-test-slug";
+  num = 18;
+  date = "2026-07-16";
+  timestamp = "2026-07-16T20:33:06Z";
+  message = ''
+    THIS IS A TEST NEWS ENTRY.
+
+    This news entry was generated in gigpkgs via the gignews post command. It is not a real news entry.'';
+}
