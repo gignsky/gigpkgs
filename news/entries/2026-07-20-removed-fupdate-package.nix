82519bc9a28688f58fe2f05f142be323353abd5c
Maxwell Rupp <maxwell@giglab.dev>
2026-07-20 19:03:13 -0400

    added gignews entry for fupdate removal

---
+++ news/entries/2026-07-20-removed-fupdate-package.nix
@@ -0,0 +1,12 @@
+{
+  id = "2026-07-20-removed-fupdate-package";
+  num = 22;
+  date = "2026-07-20";
+  timestamp = "2026-07-20T23:02:07Z";
+  message = ''
+    The 'fupdate' package has been removed.
+
+    This has been done to prepare the repo for cloning in consumers as a bare package. Fupdate will
+    be reinstalled in due time (and possible added to the cachix cache).
+  '';
+}
