dbd377896d8cafb8f3433f4a16bfa9129c34ee6f
Maxwell Rupp <maxwell@giglab.dev>
2026-09-15 15:17:22 -0400

    added gignews update entry

---
+++ news/entries/2026-09-15-gignews-updated-to-0-1-3.nix
@@ -0,0 +1,9 @@
+{
+  id = "2026-09-15-gignews-updated-to-0-1-3";
+  num = 32;
+  date = "2026-09-15";
+  timestamp = "2026-09-15T19:16:54Z";
+  message = ''
+    Gignews has been updated to fix some bugs, now rockin' version 0.1.3
+  '';
+}
