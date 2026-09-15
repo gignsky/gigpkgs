118ed66e60334e07347c0b21993cf11f7e8e69bd
Maxwell Rupp <maxwell@giglab.dev>
2026-09-15 14:36:30 -0400

    inputMan: update input claude-desktop

---
+++ news/entries/2026-09-15-inputman-update-claude-desktop.nix
@@ -0,0 +1,10 @@
+{
+  id = "2026-09-15-inputman-update-claude-desktop";
+  num = 31;
+  date = "2026-09-15";
+  message = ''
+    Updated flake input 'claude-desktop'
+
+    The flake.lock entry for this input was refreshed by inputMan.
+  '';
+}
