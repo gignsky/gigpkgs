73310939f52d6f122871bb2a85f5e1e0496ac088
Maxwell Rupp <maxwell@giglab.dev>
2026-09-18 21:26:07 -0400

    inputMan: update input roll-flow

---
+++ news/entries/2026-09-18-inputman-update-roll-flow.nix
@@ -0,0 +1,10 @@
+{
+  id = "2026-09-18-inputman-update-roll-flow";
+  num = 37;
+  date = "2026-09-18";
+  message = ''
+    Updated flake input 'roll-flow'
+
+    The flake.lock entry for this input was refreshed by inputMan.
+  '';
+}
