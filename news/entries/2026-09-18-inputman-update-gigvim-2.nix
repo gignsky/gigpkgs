da5753db5420444cd4d45fb053abec7a5f394693
Maxwell Rupp <maxwell@giglab.dev>
2026-09-18 22:34:02 -0400

    inputMan: update input gigvim

---
+++ news/entries/2026-09-18-inputman-update-gigvim-2.nix
@@ -0,0 +1,10 @@
+{
+  id = "2026-09-18-inputman-update-gigvim-2";
+  num = 40;
+  date = "2026-09-18";
+  message = ''
+    Updated flake input 'gigvim'
+
+    The flake.lock entry for this input was refreshed by inputMan.
+  '';
+}
